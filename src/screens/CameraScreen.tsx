59282e596add8f63b7bbc38ee8609ace175ebfb4
Oliver Visoso <ovisosodev@gmail.com>
2023-11-24 11:28:32 -0600

    add functional camera with vision-camera

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,11 +1,39 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import {
+  useCameraPermission,
+  useCameraDevice,
+  useCodeScanner,
+  Camera,
+  Code,
+} from 'react-native-vision-camera';
 
 const CameraScreen = () => {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  const device = useCameraDevice('back');
+
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: (codes: Code[]) => {
+      console.log(`Scanned ${codes[0].value} codes!`);
+    },
+  });
+
+  if (device == null) {
+    return (
+      <View style={{flex: 1}}>
+        <Text> Error: No se detecto la camara</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>CameraScreen</Text>
-    </View>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      codeScanner={codeScanner}
+    />
   );
 };
 
