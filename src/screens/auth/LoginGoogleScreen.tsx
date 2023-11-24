a895fa441aead0c7134ded45f94ef01743d28abe
Oliver Visoso <ovisosodev@gmail.com>
2023-11-24 09:30:21 -0600

    add screens

---
+++ src/screens/auth/LoginGoogleScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const LoginGoogleScreen = () => {
+  return (
+    <View>
+      <Text>LoginGoogleScreen</Text>
+    </View>
+  );
+};
+
+export default LoginGoogleScreen;
